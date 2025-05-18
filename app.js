389d7a3b5dcd35d8e229455124288deb50575217
Afwans <100892135+Afwans@users.noreply.github.com>
2025-05-17 22:36:33 -0400

    Update app.js

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.use(expressLayouts);
-app.set('layout', 'baseLayout'); // ✅ default layout
+app.set('layout', 'baseLayout');
 app.use(express.static(path.join(__dirname, 'public')));
 
 
@@ -43,7 +43,6 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(cors());
-app.use(express.static(path.join(__dirname, 'public')));
 
 // Routes
 app.use('/', require('./routes/index'));
